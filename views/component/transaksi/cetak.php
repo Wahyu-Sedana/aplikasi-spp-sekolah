da40d3b5d8d934ec982d0aa1697ed3faf928ca40
wahyu-sedana <wsstudiocode@gmail.com>
2022-04-11 10:36:06 +0800

    update

--- views/component/transaksi/cetak.php
+++ views/component/transaksi/cetak.php
@@ -1,19 +1,34 @@
 <?php include_once '../../../utils/processing.php';?>
-<?php include_once '../templates/sidebar.php'?>
-<?php include_once '../templates/navbar.php'?>
-
 <?php
 
 $bulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Dashboard | <?php echo $_SESSION['username']; ?></title>
+
+    <!-- Custom fonts for this template-->
+    <link href="../../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="../../../assets/css/sb-admin-2.min.css" rel="stylesheet">
+</head>
+<body onload="getLaporan()">
+
 <div class="container-fluid">
 
-<h1 class="h3 mb-4 text-gray-800">Riwayat Pembayaran</h1>
 
             <div class="card shadow mb-4">
                 <div class="card-body">
-                <a href="cetak.php" type="button" class="btn btn-primary mb-3" target="blank"><i class="fas fa-print"></i> Cetak</a>
                     <div class="table-responsive">
                         <table class=" table table-bordered" id="dataTable" width="100%" cellspacing="0">
                             <thead>
@@ -64,6 +79,9 @@ $bulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agust
 <script>
     function getLaporan(){
         window.print();
+        
     }
 </script>
-<?php include_once '../templates/footer.php'?>
+</body>
+</html>
+
